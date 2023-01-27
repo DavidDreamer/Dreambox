e57917e578af6e541affe1c01d1b21f838fdcf8f
DavidDreamer <davidb.gamedev@gmail.com>
2023-01-28 01:02:15 +0300

    initial commit

---
+++ Yandex.Ads.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace YandexSDK
+{
+	public partial class Yandex
+	{
+		[DllImport("__Internal")]
+		public static extern void ShowFullscreenAdv();
+
+		private UniTaskCompletionSource AdvCompletionSource { get; set; }
+		
+		public async UniTask ShowFullscreenAdvAsync(CancellationToken token)
+		{
+			AdvCompletionSource = new UniTaskCompletionSource();
+			token.Register(() => AdvCompletionSource.TrySetCanceled());
+			ShowFullscreenAdv();
+			await AdvCompletionSource.Task;
+		}
+
+		[UsedImplicitly]
+		public void OnFullscrenAdvClosed() => AdvCompletionSource.TrySetResult();
+		
+		[UsedImplicitly]
+		public void OnFullscrenAdvError() => AdvCompletionSource.TrySetResult();
+	}
+}
---
+++ Yandex.cs
@@ -0,0 +1,36 @@
+using System.Runtime.InteropServices;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace YandexSDK
+{
+	public partial class Yandex: MonoBehaviour
+	{
+		[DllImport("__Internal")]
+		public static extern void SavePlayerData(string data);
+
+		[DllImport("__Internal")]
+		public static extern void LoadPlayerData();
+
+		[DllImport("__Internal")]
+		public static extern void UpdateLeaderboard(string name, int value);
+
+		[DllImport("__Internal")]
+		public static extern void RateGame();
+
+		private UniTaskCompletionSource<string> CompletionSource { get; set; }
+
+		public async UniTask<string> LoadPlayerDataAsync(CancellationToken token)
+		{
+			CompletionSource = new UniTaskCompletionSource<string>();
+			token.Register(() => CompletionSource.TrySetCanceled());
+			LoadPlayerData();
+			return await CompletionSource.Task;
+		}
+
+		[UsedImplicitly]
+		public void SetPlayerData(string data) => CompletionSource.TrySetResult(data);
+	}
+}
