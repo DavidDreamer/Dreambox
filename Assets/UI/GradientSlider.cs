dd73fa417ccfbb5752bd557106f0d75ede212fca
DavidDreamer <davidb.gamedev@gmail.com>
2023-10-28 01:16:00 +0300

    gradient slider added

---
+++ Assets/UI/GradientSlider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dreambox.UI
+{
+	[ExecuteAlways]
+	[RequireComponent(typeof(Slider))]
+	public class GradientSlider: MonoBehaviour
+	{
+		[field: SerializeField]
+		private Gradient Gradient { get; set; }
+
+		[field: SerializeField]
+		[field: HideInInspector]
+		private Slider Slider { get; set; }
+
+		[field: SerializeField]
+		[field: HideInInspector]
+		private Image FillImage { get; set; }
+
+		private void OnValidate()
+		{
+			Slider = GetComponent<Slider>();
+			FillImage = Slider.fillRect.GetComponent<Image>();
+		}
+
+		private void LateUpdate()
+		{
+			FillImage.color = Gradient.Evaluate(Slider.normalizedValue);
+		}
+	}
+}
