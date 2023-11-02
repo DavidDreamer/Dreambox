c5ec90af3d23036ccd075e4362d9b3d38770034c
DavidDreamer <davidb.gamedev@gmail.com>
2023-11-03 02:22:16 +0300

    overridable inspector added

---
+++ Assets/Core/Editor/OverridableInspector.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Saber BGS 2023. All rights reserved.
+// ---------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace DCD.Editor
+{
+	public abstract class OverridableInspector: UnityEditor.Editor
+	{
+		private const string ScriptName = "m_Script";
+
+		protected abstract IEnumerable<string> CustomProperties { get; }
+		
+		public override void OnInspectorGUI()
+		{
+			serializedObject.UpdateIfRequiredOrScript();
+			
+			using (SerializedProperty iterator = serializedObject.GetIterator())
+			{
+				bool expanded = true;
+				while (iterator.NextVisible(expanded))
+				{
+					expanded = false;
+
+					if (iterator.name == ScriptName)
+					{
+						using (new EditorGUI.DisabledScope(true))
+						{
+							EditorGUILayout.PropertyField(iterator, true);
+						}
+						
+						continue;
+					}
+					
+					if (CustomProperties.Contains(iterator.name))
+					{
+						DrawCustomProperty(iterator);
+					}
+					else
+					{
+						EditorGUILayout.PropertyField(iterator, true);
+					}
+				}
+			}
+
+			DrawAdditionalOptions();
+			
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		protected abstract void DrawCustomProperty(SerializedProperty property);
+		
+		protected virtual void DrawAdditionalOptions()
+		{
+		}
+	}
+}
