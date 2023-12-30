46be6eca0b33098c7a0e23964d070c186c65c8f4
DavidDreamer <davidb.gamedev@gmail.com>
2023-12-30 22:41:03 +0300

    target collecting refactoring

---
+++ Assets/Omniverse/Abilities/Description/Actions/CollectUnitTargets/CollectUnitTargetsFromSphere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Omniverse.Abilities.Description
+{
+	[Serializable]
+	public class CollectUnitTargetsFromSphere: CollectUnitTargets
+	{
+		[field: SerializeField]
+		private float Radius { get; set; }
+
+		public override IEnumerable<Unit> GetUnits(AbilityContext context)
+		{
+			Vector3 position = context.Points.First();
+			return PhysicsHelper.GetUnitsInSphere(position, Radius, LayerMask);
+		}
+	}
+}
