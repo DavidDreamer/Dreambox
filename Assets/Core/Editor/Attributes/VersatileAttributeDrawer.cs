e60ddf6e14e17bbf1b7b73d3b61c4e087407765a
DavidDreamer <davidb.gamedev@gmail.com>
2024-01-04 10:45:57 +0300

    indention improved

--- Assets/Core/Editor/Attributes/VersatileAttributeDrawer.cs
+++ Assets/Core/Editor/Attributes/VersatileAttributeDrawer.cs
@@ -10,31 +10,37 @@ namespace Dreambox.Core.Editor
 	{
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			var versatileTypeAttribute = (VersatileAttribute)attribute;
-			var inheritedTypes = versatileTypeAttribute.Type.GetInheritedTypes();
-
-			var displayedOptions =
-				inheritedTypes.Select(type => new GUIContent(type.Name.SplitCamelCaseWithSpaces())).ToArray();
-
-			int selectedOption = property.managedReferenceValue is null
-				? 0
-				: inheritedTypes.IndexOf(property.managedReferenceValue.GetType());
-		
-			var selectorPosition = new Rect(position)
+			using (new EditorGUI.IndentLevelScope(-1))
 			{
-				height = EditorGUIUtility.singleLineHeight
-			};
+				var versatileTypeAttribute = (VersatileAttribute)attribute;
+				var inheritedTypes = versatileTypeAttribute.Type.GetInheritedTypes();
 
-			selectedOption = EditorGUI.Popup(selectorPosition, label, selectedOption, displayedOptions);
+				var displayedOptions =
+					inheritedTypes.Select(type => new GUIContent(type.Name.SplitCamelCaseWithSpaces())).ToArray();
 
-			Type selectedType = inheritedTypes[selectedOption];
+				int selectedOption = property.managedReferenceValue is null
+					? 0
+					: inheritedTypes.IndexOf(property.managedReferenceValue.GetType());
 
-			if (property.managedReferenceValue is null || property.managedReferenceValue.GetType() != selectedType)
-			{
-				property.managedReferenceValue = Activator.CreateInstance(selectedType);
+				var selectorPosition = new Rect(position)
+				{
+					height = EditorGUIUtility.singleLineHeight
+				};
+
+				using (new EditorGUI.IndentLevelScope(1))
+				{
+					selectedOption = EditorGUI.Popup(selectorPosition, label, selectedOption, displayedOptions);
+				}
+
+				Type selectedType = inheritedTypes[selectedOption];
+
+				if (property.managedReferenceValue is null || property.managedReferenceValue.GetType() != selectedType)
+				{
+					property.managedReferenceValue = Activator.CreateInstance(selectedType);
+				}
+
+				EditorGUI.PropertyField(position, property, GUIContent.none, true);
 			}
-			
-			EditorGUI.PropertyField(position, property, GUIContent.none, true);
 		}
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label) =>
