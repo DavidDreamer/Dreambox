3044f782606a4f9f2e7dbbba49e8efe68f578823
DavidDreamer <davidb.gamedev@gmail.com>
2024-01-05 08:06:41 +0300

    preloaded scriptable object added

---
+++ Assets/Core/Utils/PreloadedScriptableObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Dreambox.Core
+{
+	public class PreloadedScriptableObject<T>: ScriptableObject where T : PreloadedScriptableObject<T>
+	{
+		public static T Instance { get; private set; }
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			var preloadedAssets = UnityEditor.PlayerSettings.GetPreloadedAssets();
+			if (preloadedAssets.Contains(this))
+			{
+				return;
+			}
+
+			var tempList = preloadedAssets.ToList();
+			tempList.Add(this);
+			preloadedAssets = tempList.ToArray();
+
+			UnityEditor.PlayerSettings.SetPreloadedAssets(preloadedAssets);
+		}
+#endif
+
+		private void OnEnable()
+		{
+			if (Instance != null && Instance != this)
+			{
+				throw new Exception($"Multiple instances of {typeof(T).Name} detected");
+			}
+
+			Instance = (T)this;
+		}
+	}
+}
