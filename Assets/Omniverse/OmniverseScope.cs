00bbd8eeb6483a2c7cae7b9c69c6677f6660292f
DavidDreamer <davidb.gamedev@gmail.com>
2024-01-19 00:22:38 +0300

    OmniverseScope added

---
+++ Assets/Omniverse/OmniverseScope.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Dreambox.Core.Pooling;
+using VContainer;
+using VContainer.Unity;
+
+namespace Omniverse
+{
+	public class OmniverseScope: LifetimeScope
+	{
+		protected override void Configure(IContainerBuilder builder)
+		{
+			builder.Register<PrefabPool>(Lifetime.Singleton);
+			builder.RegisterEntryPoint<FactionManager>().AsSelf().WithParameter(GlobalSettings.Instance.Factions.ToList());
+			builder.RegisterEntryPoint<ItemManager>().AsSelf();
+			builder.RegisterEntryPoint<UnitManager>().AsSelf();
+		}
+	}
+}
