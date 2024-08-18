502df10bbe737051aba68fc16c9976d704c77ca0
DavidDreamer <davidb.gamedev@gmail.com>
2024-08-18 08:46:08 +0300

    custom renderer config added

---
+++ Assets/Rendering/URP/CustomRenderer/CustomRendererConfig.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Dreambox.Rendering.URP
+{
+	public class CustomRendererConfig : ScriptableObject
+	{
+
+	}
+}
