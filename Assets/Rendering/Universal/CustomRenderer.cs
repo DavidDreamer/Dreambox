cdbe753b8a7bdd1a40632a69d32368a5169c8903
DavidDreamer <davidb.gamedev@gmail.com>
2024-08-21 02:44:55 +0300

    refactoring

--- Assets/Rendering/Universal/CustomRenderer.cs
+++ Assets/Rendering/Universal/CustomRenderer.cs
@@ -2,31 +2,40 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using VContainer;
+using VContainer.Unity;
 
 namespace Dreambox.Rendering.Universal
 {
-	public abstract class CustomRenderer<TConfig, TPass> : MonoBehaviour
+	public abstract class CustomRenderer<TConfig, TPass> : MonoBehaviour, IInitializable, IDisposable
 		where TConfig : CustomRendererConfig
 		where TPass : ScriptableRenderPass, IDisposable
 	{
-		[field: SerializeField]
+		[Inject]
 		public TConfig Config { get; private set; }
 
 		private TPass Pass { get; set; }
 
 		protected abstract TPass CreatePass();
 
-		private void OnEnable()
+		public virtual void Initialize()
 		{
 			Pass = CreatePass();
 			Pass.renderPassEvent = Config.RenderPassEvent;
+		}
 
+		public virtual void Dispose()
+		{
+			Pass.Dispose();
+		}
+
+		private void OnEnable()
+		{
 			RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
 		}
 
 		private void OnDisable()
 		{
-			Pass.Dispose();
 
 			RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
 		}
