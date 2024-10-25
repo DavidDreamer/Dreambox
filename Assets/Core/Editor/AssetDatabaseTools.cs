940e52cf202db7a1f8172ae33c61a48e97abd21e
DavidDreamer <davidb.gamedev@gmail.com>
2024-10-25 20:12:28 +0300

    AssetDatabaseTools added

---
+++ Assets/Core/Editor/AssetDatabaseTools.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Dreambox.Core.Editor
+{
+	public static class AssetDatabaseTools
+	{
+		[MenuItem("Dreambox/Assets/Remove Scriptable Objects With Missing Script")]
+		public static void RemoveScriptableObjectsWithMissingScript()
+		{
+			Object[] assets = Selection.objects;
+			foreach (Object asset in assets)
+			{
+				string assetPath = AssetDatabase.GetAssetPath(asset);
+				AssetDatabase.RemoveScriptableObjectsWithMissingScript(assetPath);
+			}
+		}
+
+		[MenuItem("Dreambox/Assets/Delete Selected SubAssets")]
+		public static void DeleteSelectedSubAssets()
+		{
+			Object[] assets = Selection.objects;
+			foreach (Object asset in assets)
+			{
+				if (!AssetDatabase.IsSubAsset(asset))
+				{
+					continue;
+				}
+
+				AssetDatabase.RemoveObjectFromAsset(asset);
+
+				string assetPath = AssetDatabase.GetAssetPath(asset);
+				AssetDatabase.ImportAsset(assetPath);
+			}
+		}
+	}
+}
